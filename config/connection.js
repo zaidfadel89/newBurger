cca55da1c5dcc1b17a4beabe2bd282ccddd7fab1
Zaid Fadel <zaid.fadel89@gmail.com>
2019-11-22 13:38:20 -0600

    burgers

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
+    host: "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bm51qe4telbc1psg",
+    password: "oqzwpu6yz0ytacol",
+    database: "sscx0yp50gwa1thu"
   });
 }
 connection.connect();
